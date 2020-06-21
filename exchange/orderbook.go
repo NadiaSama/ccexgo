9257ceed0e536f38f93e5b59e956b4721261a92d
emanon <notexist@not.exist>
2020-06-21 23:06:02 +0800

    add orderbook impl

--- exchange/orderbook.go
+++ exchange/orderbook.go
@@ -1,5 +1,13 @@
 package exchange
 
+import (
+	"time"
+
+	"github.com/NadiaSama/ccexgo/misc/float"
+	"github.com/emirpasic/gods/trees/btree"
+	"github.com/emirpasic/gods/utils"
+)
+
 type (
 	OrderElem struct {
 		Price  float64
@@ -13,4 +21,94 @@ type (
 		Bids   []OrderElem
 		Asks   []OrderElem
 	}
+
+	OrderBookDS struct {
+		symbol  string
+		bids    *btree.Tree
+		asks    *btree.Tree
+		updated time.Time
+	}
+
+	OrderBook struct {
+		Symbol  string
+		Bids    []OrderElem
+		Asks    []OrderElem
+		Created time.Time
+	}
 )
+
+func NewOrderBookDS(symbol string, bids []OrderElem, asks []OrderElem) *OrderBookDS {
+	return &OrderBookDS{
+		symbol:  symbol,
+		bids:    newBook(bids),
+		asks:    newBook(asks),
+		updated: time.Now(),
+	}
+}
+
+func newBook(data []OrderElem) *btree.Tree {
+	l := len(data)
+	if l < 3 {
+		l = 3
+	}
+	tree := btree.NewWith(l, utils.Float64Comparator)
+	for _, depth := range data {
+		if float.Equal(depth.Price, 0.0) {
+			continue
+		}
+		tree.Put(depth.Price, depth.Amount)
+	}
+	return tree
+}
+
+func (ds *OrderBookDS) Update(notify *OrderBookNotify) {
+	updateTree := func(dest *btree.Tree, src []OrderElem) {
+		for _, elem := range src {
+			if float.Equal(elem.Price, 0.0) {
+				continue
+			}
+
+			if float.Equal(elem.Amount, 0.0) {
+				if _, ok := dest.Get(elem.Price); !ok {
+					continue
+				}
+				dest.Remove(elem.Price)
+			} else {
+				dest.Put(elem.Price, elem.Amount)
+			}
+		}
+	}
+
+	updateTree(ds.bids, notify.Bids)
+	updateTree(ds.asks, notify.Asks)
+	ds.updated = time.Now()
+}
+
+func (ds *OrderBookDS) Snapshot() *OrderBook {
+	ret := &OrderBook{
+		Symbol:  ds.symbol,
+		Bids:    make([]OrderElem, ds.bids.Size()),
+		Asks:    make([]OrderElem, ds.asks.Size()),
+		Created: ds.updated,
+	}
+
+	biter := ds.bids.Iterator()
+	biter.End()
+	i := 0
+	for biter.Prev() {
+		ret.Bids[i].Price = biter.Key().(float64)
+		ret.Bids[i].Amount = biter.Value().(float64)
+		i++
+	}
+
+	aiter := ds.asks.Iterator()
+	aiter.Begin()
+	i = 0
+	for aiter.Next() {
+		ret.Asks[i].Price = aiter.Key().(float64)
+		ret.Asks[i].Amount = aiter.Value().(float64)
+		i++
+	}
+
+	return ret
+}
---
+++ exchange/orderbook_test.go
@@ -0,0 +1,19 @@
+package exchange
+
+import "testing"
+
+func TestOrderBook(t *testing.T) {
+	ods := NewOrderBookDS("A", []OrderElem{}, []OrderElem{})
+
+	ods.Update(&OrderBookNotify{
+		Symbol: "A",
+		Bids:   []OrderElem{{1.0, 2.0}, {0.0, 1.0}, {0.5, 1.0}},
+		Asks:   []OrderElem{{10.0, 1.0}, {3.0, 1.0}},
+	})
+
+	book := ods.Snapshot()
+	if len(book.Bids) != 2 || book.Bids[0].Price != 1.0 || book.Bids[1].Price != 0.5 ||
+		book.Asks[0].Price != 3.0 || book.Asks[1].Price != 10.0 {
+		t.Errorf("bad snapshot %v", *book)
+	}
+}
