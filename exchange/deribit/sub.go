d1092b9ff8de912b459d34ba080725df298e2c1f
wang yue home windows <wangyue@clickwise.cn>
2020-06-22 14:33:10 +0800

    1.add subscribe logic

---
+++ exchange/deribit/sub.go
@@ -0,0 +1,31 @@
+package deribit
+
+import (
+	"context"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func (c *Client) Subscribe(ctx context.Context, channels ...string) error {
+	var result []string
+	if err := c.call(ctx, "public/subscribe", map[string]interface{}{
+		"channels": channels,
+	}, &result, false); err != nil {
+		return err
+	}
+
+	if len(result) != len(channels) {
+		return errors.Errorf("subscribe error bad result %s", strings.Join(result, ","))
+	}
+	set := map[string]struct{}{}
+	for _, r := range result {
+		set[r] = struct{}{}
+	}
+	for _, r := range channels {
+		if _, ok := set[r]; !ok {
+			return errors.Errorf("failed subscribe channel %s", r)
+		}
+	}
+	return nil
+}
