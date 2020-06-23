38c7b5003ef8a8b701db4aee690fbe4ce1863657
wang yue home windows <wangyue@clickwise.cn>
2020-06-23 18:45:35 +0800

    1.add unsubscribe

--- exchange/deribit/sub.go
+++ exchange/deribit/sub.go
@@ -2,22 +2,32 @@ package deribit
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
 func (c *Client) Subscribe(ctx context.Context, channels ...string) error {
+	return c.subInternal(ctx, "subscribe", channels...)
+}
+
+func (c *Client) UnSubscribe(ctx context.Context, channels ...string) error {
+	return c.subInternal(ctx, "unsubscribe", channels...)
+}
+
+func (c *Client) subInternal(ctx context.Context, op string, channels ...string) error {
 	var result []string
-	if err := c.call(ctx, "public/subscribe", map[string]interface{}{
+	method := fmt.Sprintf("public/%s", op)
+	if err := c.call(ctx, method, map[string]interface{}{
 		"channels": channels,
 	}, &result, false); err != nil {
 		return err
 	}
 
 	if len(result) != len(channels) {
-		return errors.Errorf("subscribe [%s] error bad result [%s]",
-			strings.Join(channels, ","), strings.Join(result, ","))
+		return errors.Errorf("%s [%s] error bad result [%s]",
+			op, strings.Join(channels, ","), strings.Join(result, ","))
 	}
 	set := map[string]struct{}{}
 	for _, r := range result {
@@ -25,7 +35,7 @@ func (c *Client) Subscribe(ctx context.Context, channels ...string) error {
 	}
 	for _, r := range channels {
 		if _, ok := set[r]; !ok {
-			return errors.Errorf("failed subscribe channel %s", r)
+			return errors.Errorf("failed %s channel %s", op, r)
 		}
 	}
 	return nil
