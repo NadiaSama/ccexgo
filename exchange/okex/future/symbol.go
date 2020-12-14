4dc49cfbca729780a00008775e043107cf9b6894
emanon <notexist@not.exist>
2020-12-14 19:13:14 +0800

    add okex futures symbol init and ticker logic

--- exchange/okex/future/symbol.go
+++ exchange/okex/future/symbol.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sort"
+	"sync"
+	"time"
 
 	"github.com/NadiaSama/ccexgo/exchange"
 	"github.com/NadiaSama/ccexgo/exchange/okex"
@@ -46,15 +49,18 @@ var (
 		"quarter":    exchange.FutureTypeCQ,
 		"bi_quarter": exchange.FutureTypeNQ,
 	}
+
+	symbolMapMu                    = sync.Mutex{}
+	symbolMap   map[string]*Symbol = map[string]*Symbol{}
 )
 
-func (rc *RestClient) Symbols(ctx context.Context) ([]exchange.Symbol, error) {
+func (rc *RestClient) Symbols(ctx context.Context) ([]exchange.FuturesSymbol, error) {
 	symbols, err := rc.RawSymbols(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	ret := make([]exchange.Symbol, len(symbols))
+	ret := make([]exchange.FuturesSymbol, len(symbols))
 	for i, raw := range symbols {
 		s, err := raw.Parse()
 		if err != nil {
@@ -80,7 +86,7 @@ func (os *OkexSymbol) Parse() (*Symbol, error) {
 		return nil, errors.Errorf("unkown alias='%s'", os.Alias)
 	}
 
-	dt := fmt.Sprintf("%sT08:00:00.000Z")
+	dt := fmt.Sprintf("%sT08:00:00.000Z", os.Delivery)
 	st, err := okex.ParseTime(dt)
 	if err != nil {
 		return nil, err
@@ -94,4 +100,64 @@ func (os *OkexSymbol) Parse() (*Symbol, error) {
 func (s *Symbol) String() string {
 	st := s.SettleTime()
 	return fmt.Sprintf("%s-%s", s.Index(), st.Format("060102"))
+}
+
+//Init start a background goroutine which used to update symbol map
+func Init(ctx context.Context) error {
+	next, err := updateSymbolMap(ctx)
+	if err != nil {
+		return err
+	}
+
+	updateTimer := time.NewTimer(time.Until(next))
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case <-updateTimer.C:
+				if n, err := updateSymbolMap(ctx); err == nil {
+					updateTimer = time.NewTimer(time.Until(n))
+				}
+			}
+		}
+	}()
+	return nil
+}
+
+func ParseSymbol(symbol string) (exchange.FuturesSymbol, error) {
+	symbolMapMu.Lock()
+	defer symbolMapMu.Unlock()
+	sym, ok := symbolMap[symbol]
+	if !ok {
+		return nil, errors.Errorf("unkown symbol '%s'", symbol)
+	}
+
+	return sym, nil
+}
+
+func updateSymbolMap(ctx context.Context) (time.Time, error) {
+	client := RestClient{}
+	symbols, err := client.Symbols(ctx)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	sort.Slice(symbols, func(i, j int) bool {
+		ti := symbols[i].SettleTime()
+		tj := symbols[j].SettleTime()
+		return ti.Before(tj)
+	})
+
+	m := map[string]*Symbol{}
+	for _, s := range symbols {
+		m[s.String()] = s.(*Symbol)
+	}
+	symbolMapMu.Lock()
+	defer symbolMapMu.Unlock()
+	symbolMap = m
+
+	return symbols[0].SettleTime(), nil
 }
---
+++ exchange/okex/future/ticker.go
@@ -0,0 +1,110 @@
+package future
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/NadiaSama/ccexgo/exchange"
+	"github.com/NadiaSama/ccexgo/exchange/okex"
+	"github.com/NadiaSama/ccexgo/internal/rpc"
+	"github.com/shopspring/decimal"
+)
+
+type (
+	tickerChannel struct {
+		symbol exchange.FuturesSymbol
+	}
+
+	rawTicker struct {
+		Last           decimal.Decimal `json:"last"`
+		Open24H        decimal.Decimal `json:"open_24h"`
+		BestBid        decimal.Decimal `json:"best_bid"`
+		High24H        decimal.Decimal `json:"high_24h"`
+		Low24H         decimal.Decimal `json:"low_24h"`
+		Volume24H      decimal.Decimal `json:"volume_24h"`
+		VolumeToken24H decimal.Decimal `json:"volume_token_24h"`
+		BestAsk        decimal.Decimal `json:"best_ask"`
+		OpenInterest   decimal.Decimal `json:"open_interest"`
+		InstrumentID   string          `json:"instrument_id"`
+		Timestamp      string          `json:"timestamp"`
+		BestBidSize    decimal.Decimal `json:"best_bid_size"`
+		BestAskSize    decimal.Decimal `json:"best_ask_size"`
+		LastQty        decimal.Decimal `json:"last_qty"`
+	}
+
+	Ticker struct {
+		Symbol         exchange.FuturesSymbol
+		Last           decimal.Decimal
+		LastQty        decimal.Decimal
+		BestBid        decimal.Decimal
+		BestBidSize    decimal.Decimal
+		BestAsk        decimal.Decimal
+		BestAskSize    decimal.Decimal
+		Open24H        decimal.Decimal
+		High24H        decimal.Decimal
+		Low24H         decimal.Decimal
+		Volume24H      decimal.Decimal
+		VolumeToken24H decimal.Decimal
+		OpenInterest   decimal.Decimal
+		Time           time.Time
+	}
+)
+
+const (
+	TickerTable = "futures/ticker"
+)
+
+func init() {
+	okex.SubscribeCB(TickerTable, parseTickerCB)
+}
+
+func NewTickerChannel(symbol exchange.FuturesSymbol) exchange.Channel {
+	return &tickerChannel{
+		symbol: symbol,
+	}
+}
+
+func (tc *tickerChannel) String() string {
+	return fmt.Sprintf("%s:%s", TickerTable, tc.symbol.String())
+}
+
+func parseTickerCB(table string, action string, raw json.RawMessage) (*rpc.Notify, error) {
+	var rt []rawTicker
+	if err := json.Unmarshal(raw, &rt); err != nil {
+		return nil, err
+	}
+
+	r := rt[0]
+	sym, err := ParseSymbol(r.InstrumentID)
+	if err != nil {
+		return nil, err
+	}
+
+	ts, err := okex.ParseTime(r.Timestamp)
+	if err != nil {
+		return nil, err
+	}
+
+	ticker := Ticker{
+		Symbol:         sym,
+		Last:           r.Last,
+		LastQty:        r.LastQty,
+		BestBid:        r.BestBid,
+		BestBidSize:    r.BestBidSize,
+		BestAsk:        r.BestAsk,
+		BestAskSize:    r.BestAskSize,
+		Open24H:        r.Open24H,
+		High24H:        r.High24H,
+		Low24H:         r.Low24H,
+		Volume24H:      r.Volume24H,
+		VolumeToken24H: r.VolumeToken24H,
+		OpenInterest:   r.OpenInterest,
+		Time:           ts,
+	}
+
+	return &rpc.Notify{
+		Method: TickerTable,
+		Params: &ticker,
+	}, nil
+}
