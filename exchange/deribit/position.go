4551c1df3cda5b92128bbd0d901b36d664dce54c
= <=wangyue@digifinex.com>
2021-02-06 15:53:43 +0800

    add deribit positions request

---
+++ exchange/deribit/position.go
@@ -0,0 +1,43 @@
+package deribit
+
+import "github.com/shopspring/decimal"
+
+type (
+	PositionResult struct {
+		AveragePrice              decimal.Decimal `json:"average_price"`
+		AveragePriceUSD           decimal.Decimal `json:"average_price_usd"`
+		Delta                     decimal.Decimal `json:"delta"`
+		Direction                 string          `json:"direction"`
+		EstimatedLiquidationPrice decimal.Decimal `json:"estimated_liquidation_price"`
+		FloatingProfitLoss        decimal.Decimal `json:"floating_profit_loss"`
+		FloatingProfitLossUSD     decimal.Decimal `json:"floating_profit_loss_usd"`
+		Gamma                     decimal.Decimal `json:"gamma"`
+		IndexPrice                decimal.Decimal `json:"index_price"`
+		InitialMargin             decimal.Decimal `json:"initial_margin"`
+		InstrumentName            string          `json:"instrument_name"`
+		Kind                      string          `json:"kind"`
+		Leverage                  int             `json:"leverage"`
+		MaintenanceMargin         decimal.Decimal `json:"maintenance_margin"`
+		MarkPrice                 decimal.Decimal `json:"mark_price"`
+		OpenOrdersMargin          decimal.Decimal `json:"open_orders_margin"`
+		RealizedFunding           decimal.Decimal `json:"realized_funding"`
+		RealizedProfitLoss        decimal.Decimal `json:"realized_profit_loss"`
+		SettlementPrice           decimal.Decimal `json:"settlement_price"`
+		Size                      decimal.Decimal `json:"size"`
+		SizeCurrency              decimal.Decimal `json:"size_currency"`
+		Theta                     decimal.Decimal `json:"theta"`
+		TotalProfitLoss           decimal.Decimal `json:"total_profit_loss"`
+		Vega                      decimal.Decimal `json:"vega"`
+	}
+
+	PositionsRequest struct {
+		AuthToken
+		Currency string `json:"currency"`
+		Kind     string `json:"kind"`
+	}
+)
+
+const (
+	PrivateGetPosition  = "private/get_position"
+	PrivateGetPositions = "private/get_positions"
+)
