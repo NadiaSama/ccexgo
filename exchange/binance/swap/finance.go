519805cceefde4d51e1079e6bcf007727b6bd835
= <=wangyue@digifinex.com>
2021-05-08 11:13:58 +0800

    add binance income log support

---
+++ exchange/binance/swap/finance.go
@@ -0,0 +1,69 @@
+package swap
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+)
+
+type (
+	IncomeType string
+
+	Income struct {
+		Symbol     string          `json:"symbol"`
+		IncomeType IncomeType      `json:"incomeType"`
+		Income     decimal.Decimal `json:"income"`
+		Asset      string          `json:"asset"`
+		Info       string          `json:"info"`
+		Time       int64           `json:"time"`
+		TranID     int64           `json:"tranId"`
+		TradeID    string          `json:"tradeId"`
+	}
+)
+
+const (
+	IncomeTypeNone           IncomeType = ""
+	IncomeTypeTransfer       IncomeType = "TRANSFER"
+	IncomeTypeWelcomeBonus   IncomeType = "WELCOME_BONUS"
+	IncomeTypeRealizedPnl    IncomeType = "REALIZED_PNL"
+	IncomeTypeFundingFee     IncomeType = "FUNDING_FEE"
+	IncomeTypeCommission     IncomeType = "COMMISSION"
+	IncomeTypeInsuranceClear IncomeType = "INSURANCE_CLEAR"
+)
+
+const (
+	IncomeEndPoint = "/fapi/v1/income"
+)
+
+func (rc *RestClient) Income(ctx context.Context, symbol string, it IncomeType, st int64, et int64, limit int) ([]Income, error) {
+	values := url.Values{}
+	if symbol != "" {
+		values.Add("symbol", symbol)
+	}
+
+	if it != IncomeTypeNone {
+		values.Add("incomeType", string(it))
+	}
+
+	if st != 0 {
+		values.Add("startTime", fmt.Sprintf("%d", st))
+	}
+
+	if et != 0 {
+		values.Add("endTime", fmt.Sprintf("%d", et))
+	}
+
+	if limit != 0 {
+		values.Add("limit", fmt.Sprintf("%d", limit))
+	}
+
+	var ret []Income
+	if err := rc.Request(ctx, http.MethodGet, IncomeEndPoint, values, nil, true, &ret); err != nil {
+		return nil, errors.WithMessage(err, "get income fail")
+	}
+	return ret, nil
+}
