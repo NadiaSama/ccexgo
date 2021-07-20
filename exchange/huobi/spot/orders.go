c2cd13c9256791d81dd69b579e35fdcf8c59e553
= <=wangyue@digifinex.com>
2021-07-20 18:14:18 +0800

    add huobi fetch orders

--- exchange/huobi/spot/orders.go
+++ exchange/huobi/spot/orders.go
@@ -6,8 +6,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/NadiaSama/ccexgo/exchange"
+	"github.com/NadiaSama/ccexgo/exchange/huobi"
 	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
 )
 
 type (
@@ -15,17 +19,64 @@ type (
 		data map[string]string
 	}
 
+	OrdersReq struct {
+		OrderID string
+	}
+
+	SubmitCancelReq OrdersReq
+	MatchResultReq  OrdersReq
+
 	PlaceResp struct {
 		Status  string `json:"status"`
 		Data    string `json:"data"`
 		ErrCode string `json:"err-code"`
 		ErrMsg  string `json:"err-msg"`
 	}
+
+	OrdersRespDetail struct {
+		ID               int64  `json:"id"`
+		Symbol           string `json:"symbol"`
+		AccountID        int64  `json:"account-id"`
+		Amount           string `json:"amount"`
+		Price            string `json:"price"`
+		CreatedAt        int64  `json:"created-at"`
+		Type             string `json:"type"`
+		FilledAmount     string `json:"filled-amount"`
+		FilledCashAmount string `json:"filled-cash-amount"`
+		FilledFees       string `json:"filled-fees"`
+		FinishedAt       int64  `json:"finished-at"`
+		UserID           int64  `json:"user-id"`
+		Source           string `json:"source"`
+		State            string `json:"state"`
+		CanceledAt       int64  `json:"canceled-at"`
+	}
+
+	OrdersResp struct {
+		Data OrdersRespDetail `json:"data"`
+	}
 )
 
 const (
 	PlaceOrderEndPoint = "/v1/order/orders/place"
 )
+
+func NewOrdersReq(orderID string) *OrdersReq {
+	return &OrdersReq{
+		OrderID: orderID,
+	}
+}
+
+func NewSubmitCancelReq(orderID string) *SubmitCancelReq {
+	return &SubmitCancelReq{
+		OrderID: orderID,
+	}
+}
+
+func NewMatchResultReq(orderID string) *MatchResultReq {
+	return &MatchResultReq{
+		OrderID: orderID,
+	}
+}
 
 func NewPlaceReq(accountID string, symbol string, typ string, amount string) *PlaceReq {
 	ret := &PlaceReq{
@@ -91,8 +142,19 @@ func (rc *RestClient) Place(ctx context.Context, req *PlaceReq) (*PlaceResp, err
 	return &resp, nil
 }
 
-func (rc *RestClient) SubmitCancel(ctx context.Context, orderID string) (*PlaceResp, error) {
-	url := fmt.Sprintf("/v1/order/orders/%s/submitcancel", orderID)
+func (rc *RestClient) Orders(ctx context.Context, req *OrdersReq) (*OrdersResp, error) {
+	url := fmt.Sprintf("/v1/order/orders/%s", req.OrderID)
+
+	var resp OrdersResp
+	if err := rc.RequestWithRawResp(ctx, http.MethodGet, url, nil, nil, true, &resp); err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func (rc *RestClient) SubmitCancel(ctx context.Context, req *SubmitCancelReq) (*PlaceResp, error) {
+	url := fmt.Sprintf("/v1/order/orders/%s/submitcancel", req.OrderID)
 
 	var resp PlaceResp
 	if err := rc.RequestWithRawResp(ctx, http.MethodPost, url, nil, nil, true, &resp); err != nil {
@@ -104,4 +166,87 @@ func (rc *RestClient) SubmitCancel(ctx context.Context, orderID string) (*PlaceR
 	}
 
 	return &resp, nil
+}
+
+func (rc *RestClient) MatchResult(ctx context.Context, req *MatchResultReq) ([]MatchResult, error) {
+	endPoint := fmt.Sprintf("/v1/order/orders/%s/matchresults", req.OrderID)
+	var resp []MatchResult
+
+	if err := rc.Request(ctx, http.MethodGet, endPoint, nil, nil, true, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (rc *RestClient) FetchOrder(ctx context.Context, order *exchange.Order) (*exchange.Order, error) {
+	req := NewOrdersReq(order.ID.String())
+	resp, err := rc.Orders(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Transform()
+}
+
+func (r *OrdersResp) Transform() (*exchange.Order, error) {
+	symbol, err := ParseSymbol(r.Data.Symbol)
+	if err != nil {
+		return nil, err
+	}
+
+	amount, err := decimal.NewFromString(r.Data.Amount)
+	if err != nil {
+		return nil, err
+	}
+	price, err := decimal.NewFromString(r.Data.Price)
+	if err != nil {
+		return nil, err
+	}
+	filled, err := decimal.NewFromString(r.Data.FilledAmount)
+	if err != nil {
+		return nil, err
+	}
+	fees, err := decimal.NewFromString(r.Data.FilledFees)
+	if err != nil {
+		return nil, err
+	}
+	cost, err := decimal.NewFromString(r.Data.FilledCashAmount)
+	var avgPrice decimal.Decimal
+	if !filled.IsZero() {
+		avgPrice = cost.Div(amount)
+	}
+
+	var ut time.Time
+	ct := huobi.ParseTS(r.Data.CreatedAt)
+	if r.Data.CanceledAt != 0 {
+		ut = huobi.ParseTS(r.Data.CanceledAt)
+	} else {
+		ut = huobi.ParseTS(r.Data.FinishedAt)
+	}
+
+	status, err := ParseOrderStatus(r.Data.State)
+	if err != nil {
+		return nil, err
+	}
+
+	side, typ, err := ParseOrderType(r.Data.Type)
+	if err != nil {
+		return nil, err
+	}
+
+	return &exchange.Order{
+		ID:       exchange.NewIntID(r.Data.ID),
+		Symbol:   symbol,
+		Price:    price,
+		Amount:   amount,
+		Filled:   filled,
+		AvgPrice: avgPrice,
+		Fee:      fees,
+		Created:  ct,
+		Updated:  ut,
+		Side:     side,
+		Status:   status,
+		Type:     typ,
+		Raw:      r,
+	}, nil
 }
