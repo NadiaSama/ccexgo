c950b81567a62d423f60f613937f735658b00a4e
= <=wangyue@digifinex.com>
2021-08-31 17:18:34 +0800

    add deribit rest client

--- exchange/deribit/instrument.go
+++ exchange/deribit/instrument.go
@@ -1,1 +1,84 @@
 package deribit
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+
+	"github.com/NadiaSama/ccexgo/exchange"
+	"github.com/pkg/errors"
+)
+
+type (
+	InstrumentsRequest struct {
+		currency string
+		kind     string
+		expired  bool
+	}
+)
+
+const (
+	InstrumentsKindOption = "option"
+	InstrumentsKindFuture = "future"
+
+	InstrumentsEndPoint = "/public/get_instruments"
+)
+
+func NewInstrumentsRequest(currency string) *InstrumentsRequest {
+	return &InstrumentsRequest{
+		currency: currency,
+	}
+}
+
+func (ir *InstrumentsRequest) Expired() *InstrumentsRequest {
+	ir.expired = true
+	return ir
+}
+
+func (ir *InstrumentsRequest) Kind(kind string) *InstrumentsRequest {
+	ir.kind = kind
+	return ir
+}
+
+func (c *RestClient) Instruments(ctx context.Context, ir *InstrumentsRequest) ([]InstrumentResult, error) {
+	values := url.Values{}
+	values.Add("currency", ir.currency)
+	if ir.kind != "" {
+		values.Add("kind", ir.kind)
+	}
+
+	if ir.expired {
+		values.Add("expired", "true")
+	}
+
+	var ret []InstrumentResult
+	if err := c.Request(ctx, http.MethodGet, InstrumentsEndPoint, values, nil, false, &ret); err != nil {
+		return nil, errors.WithMessage(err, "get instruments fail")
+	}
+	return ret, nil
+}
+
+func (c *RestClient) OptionSymbols(ctx context.Context, currency string) ([]exchange.OptionSymbol, error) {
+	req := NewInstrumentsRequest(currency).Kind(InstrumentsKindOption)
+	irs, err := c.Instruments(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make([]exchange.OptionSymbol, 0)
+
+	for _, ir := range irs {
+		sym, err := ir.Symbol()
+		if err != nil {
+			return nil, err
+		}
+
+		osym, ok := sym.(exchange.OptionSymbol)
+		if !ok {
+			return nil, errors.Errorf("invalid symbol %+v", sym)
+		}
+
+		ret = append(ret, osym)
+	}
+	return ret, nil
+}
---
+++ exchange/deribit/rest_client.go
@@ -0,0 +1,76 @@
+package deribit
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
+type (
+	RestClient struct {
+		key    string
+		secret string
+		prefix string
+	}
+)
+
+func NewRestClient(key, secret string) *RestClient {
+	return newRestClientWithPrefix(key, secret, "https://www.deribit.com/api/v2")
+}
+
+func NewTestRestClient(key, secret string) *RestClient {
+	return newRestClientWithPrefix(key, secret, "https://test.deribit.com/api/v2")
+}
+
+func newRestClientWithPrefix(key, secret, prefix string) *RestClient {
+	return &RestClient{
+		key:    key,
+		secret: secret,
+		prefix: prefix,
+	}
+}
+
+func (rc *RestClient) Request(ctx context.Context, method string, endPoint string, params url.Values, body io.Reader, signed bool, dst interface{}) error {
+	if signed {
+		return errors.Errorf("signed rest request is not support yet")
+	}
+
+	url := fmt.Sprintf("%s%s", rc.prefix, endPoint)
+	if len(params) != 0 {
+		url = fmt.Sprintf("%s?%s", url, params.Encode())
+	}
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return errors.WithMessage(err, "build request fail")
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return errors.WithMessage(err, "do http request fail")
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.WithMessage(err, "read response fail")
+	}
+	defer resp.Body.Close()
+
+	var r Response
+	if err := json.Unmarshal(data, &r); err != nil {
+		return errors.WithMessage(err, "unmarshal json error")
+	}
+	if r.Error.Code != 0 {
+		return errors.Errorf("response error code: %d message: %s", r.Error.Code, r.Error.Message)
+	}
+
+	if err := json.Unmarshal(r.Result, &dst); err != nil {
+		return errors.WithMessage(err, "unmarshal Result error")
+	}
+	return nil
+}
