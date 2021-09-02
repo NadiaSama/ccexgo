3f32032dbc8020abab5573c8b984dc10e9800dbf
= <=wangyue@digifinex.com>
2021-09-02 12:17:28 +0800

    add helper method round

---
+++ misc/precision/round.go
@@ -0,0 +1,33 @@
+package round
+
+import (
+	"github.com/NadiaSama/ccexgo/exchange"
+	"github.com/shopspring/decimal"
+)
+
+func RoundAmountFloat(symbol exchange.Symbol, amount float64) float64 {
+	return roundFloat(symbol.AmountPrecision(), amount)
+}
+
+func RoundAmount(symbol exchange.Symbol, amount decimal.Decimal) decimal.Decimal {
+	return roundDecimal(symbol.AmountPrecision(), amount)
+}
+
+func RoundPrice(symbol exchange.Symbol, price decimal.Decimal) decimal.Decimal {
+	return roundDecimal(symbol.PricePrecision(), price)
+}
+
+func RoundPriceFloat(symbol exchange.Symbol, price float64) float64 {
+	return roundFloat(symbol.PricePrecision(), price)
+}
+
+func roundFloat(exp decimal.Decimal, val float64) float64 {
+	ret := roundDecimal(exp, decimal.NewFromFloat(val))
+	r, _ := ret.Float64()
+	return r
+}
+
+func roundDecimal(exp decimal.Decimal, val decimal.Decimal) decimal.Decimal {
+	e := exp.Exponent()
+	return val.Round(e * -1)
+}
