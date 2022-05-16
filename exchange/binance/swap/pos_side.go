e5ecb3b1f7ba3d38d9b9000ecebd7992e42dbaec
emanon@digifinex.com <notexist@not.exist>
2022-05-16 15:18:24 +0800

    add binance swap pos_side support

---
+++ exchange/binance/swap/pos_side.go
@@ -0,0 +1,84 @@
+package swap
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/NadiaSama/ccexgo/exchange/binance"
+	"github.com/pkg/errors"
+)
+
+type (
+	GetPositionSideRequest struct {
+		*binance.RestReq
+	}
+
+	GetPositionSideResp struct {
+		DualSidePosition bool `json:"dualSidePosition"`
+	}
+
+	SetPositionSideRequest struct {
+		*binance.RestReq
+		dualSide bool
+	}
+
+	SetPositionSideResp struct {
+		Code    int    `json:"code"`
+		Message string `json:"msg"`
+	}
+)
+
+const (
+	PositionSidePath = "/fapi/v1/positionSide/dual"
+)
+
+func (rc *RestClient) GetPositionSide(ctx context.Context, req *GetPositionSideRequest) (*GetPositionSideResp, error) {
+	if rc.side != nil {
+		return rc.side, nil
+	}
+
+	var side GetPositionSideResp
+	if err := rc.GetRequest(ctx, PositionSidePath, req, true, &side); err != nil {
+		return nil, errors.WithMessage(err, "get dual position side fail")
+	}
+
+	rc.side = &side
+	return &side, nil
+}
+
+func (rc *RestClient) SetPositionSide(ctx context.Context, req *SetPositionSideRequest) error {
+	values, err := req.Values()
+	if err != nil {
+		return errors.WithMessage(err, "get request values fail")
+	}
+
+	var resp SetPositionSideResp
+	if err := rc.Request(ctx, http.MethodPost, PositionSidePath, values, nil, true, &resp); err != nil {
+		return errors.WithMessage(err, "set position side fail")
+	}
+
+	if resp.Code != 200 {
+		return errors.Errorf("set position fail resp=%+v", resp)
+	}
+
+	rc.side = &GetPositionSideResp{
+		DualSidePosition: req.dualSide,
+	}
+
+	return nil
+}
+
+func NewGetPositionSideRequest() *GetPositionSideRequest {
+	return &GetPositionSideRequest{
+		binance.NewRestReq(),
+	}
+}
+
+func NewSetPositionSideRequest(dualSide bool) *SetPositionSideRequest {
+	req := binance.NewRestReq()
+	req.AddFields("dualSidePosition", dualSide)
+	return &SetPositionSideRequest{
+		RestReq:  req,
+		dualSide: dualSide,
+	}
+}
