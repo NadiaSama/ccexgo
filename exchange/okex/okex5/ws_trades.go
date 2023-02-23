cd9d6f77bd982599591ac4b57a986650ef8089cd
emanon <emanon_wang@digifinex.org>
2023-02-23 15:17:02 +0800

    okex5 edit trade notify fmt

--- exchange/okex/okex5/ws_trades.go
+++ exchange/okex/okex5/ws_trades.go
@@ -3,7 +3,10 @@ package okex5
 import (
 	"encoding/json"
 
+	"github.com/NadiaSama/ccexgo/exchange"
 	"github.com/NadiaSama/ccexgo/internal/rpc"
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
 )
 
 type (
@@ -38,8 +41,57 @@ func parseTrades(data *wsResp) (*rpc.Notify, error) {
 		return nil, err
 	}
 
+	trades := []*exchange.Trade{}
+	for _, i := range t {
+		trade, err := i.Parse()
+		if err != nil {
+			return nil, errors.WithMessage(err, "parse trade fail")
+		}
+
+		trades = append(trades, trade)
+	}
+
 	return &rpc.Notify{
 		Method: data.Arg.Channel,
-		Params: t,
+		Params: trades,
+	}, nil
+}
+
+func (t *Trade) Parse() (*exchange.Trade, error) {
+	sym, err := ParseSymbol(t.InstID)
+	if err != nil {
+		return nil, errors.WithMessage(err, "parse symbol fail")
+	}
+
+	prc, err := decimal.NewFromString(t.Px)
+	if err != nil {
+		return nil, errors.WithMessage(err, "parse price fail")
+	}
+
+	amt, err := decimal.NewFromString(t.Sz)
+	if err != nil {
+		return nil, errors.WithMessage(err, "parse size fail")
+	}
+
+	ts, err := ParseTimestamp(t.Ts)
+	if err != nil {
+		return nil, errors.WithMessage(err, "parse ts fail")
+	}
+
+	var side exchange.OrderSide
+	if t.Side == OrderSideBuy {
+		side = exchange.OrderSideBuy
+	} else if t.Side == OrderSideSell {
+		side = exchange.OrderSideSell
+	}
+
+	return &exchange.Trade{
+		ID:     t.TradeID,
+		Symbol: sym,
+		Price:  prc,
+		Amount: amt,
+		Time:   ts,
+		Side:   side,
+		Raw:    t,
 	}, nil
 }
